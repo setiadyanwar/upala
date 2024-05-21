c5fc033263cff805296e4718acd51c737dda9c05
SetiadyAnwar <setiadyanwar@gmail.com>
2024-05-22 06:13:17 +0700

    hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['index.html'],
   theme: {
-    extend: {},
+    container:{
+      center:true,
+      padding:'25px',
+    },
+    extend: {
+      colors:{
+        primary:'#EF5B38',
+        state:'#FF8366',
+        dark:'#393D4E',
+        secondary:'#677487',
+      },
+      zIndex: {
+        '9999': 9999,
+      },
+      screens:{
+        '2xl':'1536px',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      }
+      
+    },
   },
   plugins: [],
 }
